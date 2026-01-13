4475bd2828bd27719b46d9513dd6c5147c4ff1b9
Anousheh Hussain <anousheh.hussain@gmail.com>
2026-01-13 21:56:26 +0530

    Fixed CORS to allow Vercel frontend

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // This will be your Frontend URL
+  origin: "https://gigflow-mern-assignment.vercel.app", // <--- The fix
   credentials: true
 }));
 
